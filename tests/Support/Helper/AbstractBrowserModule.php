8201b3cb766f7f3bf4d8e9b585f1af18b83bf55a
Roman Parpalak <roman@parpalak.com>
2024-07-05 17:29:04 +0300

    Refactored integration tests.

---
+++ tests/Support/Helper/AbstractBrowserModule.php
@@ -0,0 +1,269 @@
+<?php
+/**
+ * @copyright 2024 Roman Parpalak
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @package   S2
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Support\Helper;
+
+use Codeception\Exception\ElementNotFound;
+use Codeception\Module;
+use Symfony\Component\BrowserKit\CookieJar;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\Crawler as SymfonyCrawler;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
+use Symfony\Component\DomCrawler\Field\InputFormField;
+use Symfony\Component\DomCrawler\Field\TextareaFormField;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class AbstractBrowserModule extends Module
+{
+    protected ?Response $response = null;
+    protected ?Crawler $crawler = null;
+    protected ?CookieJar $cookieJar;
+
+    public function grabResponse(): string
+    {
+        return $this->response->getContent();
+    }
+
+    public function _initialize()
+    {
+        $this->cookieJar = new CookieJar();
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function grabJson(): ?array
+    {
+        $content = $this->response->getContent();
+        return json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    public function see(string $text, ?string $selector = null): void
+    {
+        if ($selector !== null) {
+            try {
+                $content = implode("\n", $this->crawler->filter($selector)->each(function (Crawler $node) {
+                    return $node->text();
+                }));
+            } catch (\Exception $e) {
+                $this->fail('Selector "' . $selector . '" is not found. Exception: ' . $e->getMessage());
+                return;
+            }
+        } else {
+            $content = $this->response->getContent();
+        }
+        $condition = str_contains($content, $text) || str_contains($content, htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
+        if (!$condition) {
+            $this->debug($content);
+            $this->fail('Cannot see "' . $text . '" in response');
+        }
+        $this->assertTrue($condition);
+    }
+
+    public function dontSee(string $text, ?string $selector = null): void
+    {
+        if ($selector !== null) {
+            try {
+                $content = implode("\n", $this->crawler->filter($selector)->each(function (Crawler $node) {
+                    return $node->text();
+                }));
+            } catch (\Exception $e) {
+                $this->fail('Selector "' . $selector . '" is not found. Exception: ' . $e->getMessage());
+                return;
+            }
+        } else {
+            $content = $this->response->getContent();
+        }
+        $condition = str_contains($content, $text) || str_contains($content, htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
+        if ($condition) {
+            $this->debug($content);
+            $this->fail('See "' . $text . '" in response');
+        }
+        $this->assertFalse($condition);
+    }
+
+    public function grabMultiple(string $selector): array
+    {
+        return $this->crawler->filter($selector)->each(static function (Crawler $node) {
+            return $node->text();
+        });
+    }
+
+    public function seeElement(string $selector): void
+    {
+        $condition = $this->crawler->filter($selector)->count() > 0;
+        if (!$condition) {
+            $this->debug($this->response->getContent());
+            $this->fail('Cannot see "' . $selector . '" in response');
+        }
+        $this->assertTrue($condition);
+    }
+
+    public function dontSeeElement(string $selector): void
+    {
+        $this->assertSame(0, $this->crawler->filter($selector)->count());
+    }
+
+    public function grabAttributeFrom(string $selector, string $attribute): ?string
+    {
+        return $this->crawler->filter($selector)->attr($attribute);
+    }
+
+    public function seeResponseCodeIs(int $code): void
+    {
+        $this->assertEquals($code, $this->response->getStatusCode());
+    }
+
+    public function amOnPage(string $url): void
+    {
+        $this->doRequest(Request::create($url));
+    }
+
+    public function seeLocationIs(string $location): void
+    {
+        $this->assertEquals($location, $this->response->headers->get('Location'));
+    }
+
+    public function seeLocationMatches(string $locationRegex): void
+    {
+        $this->assertMatchesRegularExpression($locationRegex, $this->response->headers->get('Location'));
+    }
+
+    public function followRedirect(): void
+    {
+        $this->doRequest(Request::create($this->response->headers->get('Location')));
+    }
+
+    public function click(string $selector): void
+    {
+        $crawler = $this->crawler->selectLink($selector);
+        if ($crawler->count() === 0) {
+            $crawler = $this->crawler->filter($selector);
+        }
+        $this->doRequest(Request::create($crawler->link()->getUri()));
+    }
+
+    public function submitForm(string $selector, array $data): void
+    {
+        $formCrawler = $this->crawler->filter($selector);
+        $button      = $formCrawler->filter('button[type="submit"]');
+        if ($button->count() > 0 && $button->attr('name')) {
+            $buttonData = [$button->attr('name') => $button->attr('value') ?? ''];
+        } else {
+            $buttonData = [];
+        }
+
+        $form = $formCrawler->form();
+        $form->disableValidation(); // see https://stackoverflow.com/questions/57386450/how-to-tick-a-specific-checkbox-from-a-multi-dimensional-field-in-a-symfony-form
+        $form->setValues($data);
+
+        // Hack for crawler. Looks like it doesn't add button data for no reason, so we do it manually here
+        $phpValues = array_merge($buttonData, $form->getPhpValues());
+        $request   = Request::create($form->getUri(), $form->getMethod(), $phpValues);
+        $this->doRequest($request);
+    }
+
+    public function sendPost(string $url, array $data): void
+    {
+        $request = Request::create($url, 'POST', $data);
+        $this->doRequest($request);
+    }
+
+    public function grabFormValues(string $selector): array
+    {
+        return $this->crawler->filter($selector)->form()->getValues();
+    }
+
+    public function grabValueFrom($field): mixed
+    {
+        $nodes = $this->crawler->filter($field);
+        if ($nodes->count() === 0) {
+            throw new ElementNotFound($field, 'Field');
+        }
+
+        if ($nodes->filter('textarea')->count() !== 0) {
+            return (new TextareaFormField($nodes->filter('textarea')->getNode(0)))->getValue();
+        }
+
+        $input = $nodes->filter('input');
+        if ($input->count() !== 0) {
+            return $this->getInputValue($input);
+        }
+
+        if ($nodes->filter('select')->count() !== 0) {
+            $field = new ChoiceFormField($nodes->filter('select')->getNode(0));
+            $options = $nodes->filter('option[selected]');
+            $values = [];
+
+            foreach ($options as $option) {
+                $values[] = $option->getAttribute('value');
+            }
+
+            if (!$field->isMultiple()) {
+                return reset($values);
+            }
+
+            return $values;
+        }
+
+        $this->fail("Element {$nodes} is not a form field or does not contain a form field");
+    }
+
+    protected function getInputValue(SymfonyCrawler $input): array|string
+    {
+        $inputType = $input->attr('type');
+        if ($inputType === 'checkbox' || $inputType === 'radio') {
+            $values = [];
+
+            foreach ($input->filter(':checked') as $checkbox) {
+                $values[] = $checkbox->getAttribute('value');
+            }
+
+            return $values;
+        }
+
+        return (new InputFormField($input->getNode(0)))->getValue();
+    }
+
+//    public function grabValueFrom(string $selector): array
+//    {
+//        $input = $this->crawler->filter($selector);
+//        $form  = $input->form();
+//        $values = $form->getValues();
+//        return $values[$input->attr('name')];
+//    }
+
+    public function grabAndMatch(string $selector, string $regex): ?array
+    {
+        $html = $this->crawler->filter($selector)->outerHtml();
+        if (preg_match($regex, $html, $matches)) {
+            return $matches;
+        }
+        return null;
+    }
+
+    protected function doRequest(Request $request): void
+    {
+        $request->cookies->replace($this->cookieJar->allValues($request->getUri()));
+        $this->debug(sprintf("%s %s", $request->getMethod(), $request->getUri()));
+        $this->response = $this->doRealRequest($request);
+        $this->cookieJar->updateFromSetCookie($this->response->headers->all('Set-Cookie'), $request->getUri());
+        $content        = $this->response->getContent();
+        $this->debug(sprintf(
+            "Get %s response:\n%s\n\n%s",
+            $this->response->getStatusCode(),
+            $this->response->headers,
+            mb_substr($content, 0, 100)
+        ));
+        $this->crawler = new Crawler($content, 'http://localhost');
+    }
+
+    abstract protected function doRealRequest(Request $request): Response;
+}
--- tests/Support/Helper/Integration.php
+++ tests/Support/Helper/Integration.php
@@ -9,25 +9,21 @@ declare(strict_types=1);
 
 namespace Tests\Support\Helper;
 
-use Codeception\Module;
 use Codeception\TestInterface;
 use Psr\Container\ContainerExceptionInterface;
 use S2\AdminYard\AdminPanel;
 use S2\AdminYard\Config\AdminConfig;
 use S2\AdminYard\DefaultAdminFactory;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
-class Integration extends Module
+class Integration extends AbstractBrowserModule
 {
     protected ?\PDO $pdo = null;
     protected ?AdminPanel $adminPanel = null;
-    protected ?Response $response = null;
-    protected ?Crawler $crawler = null;
     protected ?SessionInterface $session = null;
 
     /**
@@ -36,6 +32,8 @@ class Integration extends Module
      */
     public function _initialize(): void
     {
+        parent::_initialize();
+
         switch (getenv('APP_DB_TYPE')) {
             case 'pgsql':
                 shell_exec('sudo -u postgres psql adminyard_test < ' . __DIR__ . '/../../../demo/init_pgsql.sql');
@@ -72,101 +70,6 @@ class Integration extends Module
         return DefaultAdminFactory::createAdminPanel($adminConfig, $pdo ?? $this->pdo);
     }
 
-    public function amOnPage(string $url): void
-    {
-        $this->doRequest(Request::create($url));
-    }
-
-    public function grabResponse(): string
-    {
-        return $this->response->getContent();
-    }
-
-    /**
-     * @throws \JsonException
-     */
-    public function grabJson(): ?array
-    {
-        $content = $this->response->getContent();
-        return json_decode($content, true, 512, JSON_THROW_ON_ERROR);
-    }
-
-    public function see(string $text, ?string $selector = null): void
-    {
-        if ($selector !== null) {
-            try {
-                $content = implode("\n", $this->crawler->filter($selector)->each(function (Crawler $node) {
-                    return $node->text();
-                }));
-            } catch (\Exception $e) {
-                $this->fail('Selector "' . $selector . '" is not found. Exception: ' . $e->getMessage());
-                return;
-            }
-        } else {
-            $content = $this->response->getContent();
-        }
-        $condition = str_contains($content, $text) || str_contains($content, htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
-        if (!$condition) {
-            $this->debug($content);
-            $this->fail('Cannot see "' . $text . '" in response');
-        }
-        $this->assertTrue($condition);
-    }
-
-    public function dontSee(string $text, ?string $selector = null): void
-    {
-        if ($selector !== null) {
-            try {
-                $content = implode("\n", $this->crawler->filter($selector)->each(function (Crawler $node) {
-                    return $node->text();
-                }));
-            } catch (\Exception $e) {
-                $this->fail('Selector "' . $selector . '" is not found. Exception: ' . $e->getMessage());
-                return;
-            }
-        } else {
-            $content = $this->response->getContent();
-        }
-        $condition = str_contains($content, $text) || str_contains($content, htmlspecialchars($text, ENT_QUOTES, 'UTF-8'));
-        if ($condition) {
-            $this->debug($content);
-            $this->fail('See "' . $text . '" in response');
-        }
-        $this->assertFalse($condition);
-    }
-
-    public function grabMultiple(string $selector): array
-    {
-        return $this->crawler->filter($selector)->each(static function (Crawler $node) {
-            return $node->text();
-        });
-    }
-
-    public function seeElement(string $selector): void
-    {
-        $condition = $this->crawler->filter($selector)->count() > 0;
-        if (!$condition) {
-            $this->debug($this->response->getContent());
-            $this->fail('Cannot see "' . $selector . '" in response');
-        }
-        $this->assertTrue($condition);
-    }
-
-    public function dontSeeElement(string $selector): void
-    {
-        $this->assertSame(0, $this->crawler->filter($selector)->count());
-    }
-
-    public function grabAttributeFrom(string $selector, string $attribute): ?string
-    {
-        return $this->crawler->filter($selector)->attr($attribute);
-    }
-
-    public function seeResponseCodeIs(int $code): void
-    {
-        $this->assertEquals($code, $this->response->getStatusCode());
-    }
-
     public function seeFlashMessage(string $check): void
     {
         $flashMessages = $this->session->getFlashBag()->peekAll();
@@ -183,82 +86,9 @@ class Integration extends Module
         $this->assertTrue($condition);
     }
 
-    public function seeLocationIs(string $location): void
-    {
-        $this->assertEquals($location, $this->response->headers->get('Location'));
-    }
-
-    public function seeLocationMatches(string $locationRegex): void
-    {
-        $this->assertMatchesRegularExpression($locationRegex, $this->response->headers->get('Location'));
-    }
-
-    public function followRedirect(): void
-    {
-        $this->doRequest(Request::create($this->response->headers->get('Location')));
-    }
-
-    public function click(string $selector): void
-    {
-        $crawler = $this->crawler->selectLink($selector);
-        if ($crawler->count() === 0) {
-            $crawler = $this->crawler->filter($selector);
-        }
-        $this->doRequest(Request::create($crawler->link()->getUri()));
-    }
-
-    public function submitForm(string $selector, array $data): void
-    {
-        $formCrawler = $this->crawler->filter($selector);
-        $button      = $formCrawler->filter('button[type="submit"]');
-        if ($button->count() > 0 && $button->attr('name')) {
-            $buttonData = [$button->attr('name') => $button->attr('value') ?? ''];
-        } else {
-            $buttonData = [];
-        }
-
-        $form = $formCrawler->form();
-        $form->disableValidation(); // see https://stackoverflow.com/questions/57386450/how-to-tick-a-specific-checkbox-from-a-multi-dimensional-field-in-a-symfony-form
-        $form->setValues($data);
-
-        // Hack for crawler. Looks like it doesn't add button data for no reason, so we do it manually here
-        $phpValues = array_merge($buttonData, $form->getPhpValues());
-        $request   = Request::create($form->getUri(), $form->getMethod(), $phpValues);
-        $this->doRequest($request);
-    }
-
-    public function sendPost(string $url, array $data): void
-    {
-        $request = Request::create($url, 'POST', $data);
-        $this->doRequest($request);
-    }
-
-    public function grabFormValues(string $selector): array
-    {
-        return $this->crawler->filter($selector)->form()->getValues();
-    }
-
-    public function grabAndMatch(string $selector, string $regex): ?array
-    {
-        $html = $this->crawler->filter($selector)->outerHtml();
-        if (preg_match($regex, $html, $matches)) {
-            return $matches;
-        }
-        return null;
-    }
-
-    private function doRequest(Request $request): void
+    protected function doRealRequest(Request $request): Response
     {
         $request->setSession($this->session);
-        $this->debug(sprintf("%s %s", $request->getMethod(), $request->getUri()));
-        $this->response = $this->adminPanel->handleRequest($request);
-        $content        = $this->response->getContent();
-        $this->debug(sprintf(
-            "Get %s response:\n%s\n\n%s",
-            $this->response->getStatusCode(),
-            $this->response->headers,
-            mb_substr($content, 0, 100)
-        ));
-        $this->crawler = new Crawler($content, 'http://localhost');
+        return $this->adminPanel->handleRequest($request);
     }
 }
